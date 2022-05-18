a12c7b0256414d8f927d677738668c5fb746e9fd
Ilya <leynsboro@gmail.com>
2022-05-18 16:33:09 +0300

    Fixes + two navBar for customing

--- Contacts/ViewControllers/MainViewController.swift
+++ Contacts/ViewControllers/MainViewController.swift
@@ -15,12 +15,18 @@ class MainViewController: UITabBarController {
     }
 
     private func loadData() {
-        let firstVC = viewControllers?.first as! FirstContactViewController
-        let secondVC = viewControllers?.last as! SecondContactViewController
-        
         let persons = Person.getPersons()
-        firstVC.persons = persons
-        secondVC.persons = persons
+        
+        guard let viewControllers = viewControllers else { return }
+        
+        for viewController in viewControllers {
+            guard let navVC = viewController as? UINavigationController else { return }
+            if let firstVC = navVC.topViewController as? FirstContactViewController {
+                firstVC.persons = persons
+            } else if let secondVC = navVC.topViewController as? SecondContactViewController {
+                secondVC.persons = persons
+            }
+        }
     }
 
 
